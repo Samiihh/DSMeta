dbf58981f914fef82baaea823afc70b4df95820a
Samira Vieira <saahhlulu2@gmail.com>
2022-09-05 23:05:47 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import NotificationButton from "./components/NotificationButton"
+
+import Header from "./components/Header"
+import SalesCard from "./components/Salescard"
+
+
 
 function App() {
   return (
+
     <>
-      <h1>Olá Mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+
+          <SalesCard />
+
+          </div>
+        </section>
+      </main>
+
+
     </>
   )
 }
