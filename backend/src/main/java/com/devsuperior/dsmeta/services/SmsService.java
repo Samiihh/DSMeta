152fcf2e6a9219bd749ebb47be022b50de50ff46
Samira Vieira <saahhlulu2@gmail.com>
2022-09-11 15:42:14 -0300

    Implantação no Heroku

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -13,37 +13,37 @@ import com.twilio.type.PhoneNumber;
 @Service
 public class SmsService {
 
-	@Value("${twilio.sid}")
-	private String twilioSid;
+    @Value("${twilio.sid}")
+    private String twilioSid;
 
-	@Value("${twilio.key}")
-	private String twilioKey;
+    @Value("${twilio.key}")
+    private String twilioKey;
 
-	@Value("${twilio.phone.from}")
-	private String twilioPhoneFrom;
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
 
-	@Value("${twilio.phone.to}")
-	private String twilioPhoneTo;
-	
-	@Autowired
-	private SaleRepository saleRepository;
-	
-	public void sendSms(Long saleID) {
-		
-		Sale sale = saleRepository.findById(saleID).get();
-		
-		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear(); 
-		
-		String msg = "O Vendedor " +sale.getSellerName() + " foi destaque em " + date
-				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
 
-		Twilio.init(twilioSid, twilioKey);
+    @Autowired
+    private SaleRepository saleRepository;
 
-		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+    public void sendSms(Long saleId) {
 
-		Message message = Message.creator(to, from, msg).create();
+        Sale sale = saleRepository.findById(saleId).get();
 
-		System.out.println(message.getSid());
-	}
+        String date = sale.getDate().getMonth() + "/" + sale.getDate().getYear();
+
+        String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ "
+                + String.format("%.2f", sale.getAmount());
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg).create();
+
+        System.out.println(message.getSid());
+    }
 }
